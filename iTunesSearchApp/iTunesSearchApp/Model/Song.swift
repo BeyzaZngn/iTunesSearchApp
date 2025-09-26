ded538cb8f950ba47d14df86111c82d89b82b203
Beyza Zengin <112340720+BeyzaZngn@users.noreply.github.com>
2025-09-26 18:43:56 +0300

    Struct SongResponse and Struct Song added.

--- iTunesSearchApp/iTunesSearchApp/Model/Song.swift
+++ iTunesSearchApp/iTunesSearchApp/Model/Song.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct SongResponse: Codable { // API’den gelen cevabın tamamı. İçinde “results” diye bir liste var.
+    let results: [Song]
+}
+
+struct Song: Codable { // O listedeki bir şarkının detayları (id, isim, sanatçı, albüm kapağı).
+    let trackId: Int
+    let trackName: String
+    let artistName: String
+    let artworkUrl100: String? // Opsiyonel yapmamızın sebebi: API bazı şarkılar için görsel göndermeyebilir.
+}
+// Codable bir struct, gelen JSON verisini kolayca structa çevirebilmemizi sağlar.
